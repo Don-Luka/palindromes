d3a91431df44271c1b7e789dcf9a248d5acb28f9
Łukasz Domagalski <l.domagalski@gmail.com>
2023-08-11 09:30:59 +0200

    Added alternative version of the palindrome function

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ def palindrome(word):
 
 
 print(palindrome('maoam'))
+
+# alternatywna wersja funkcji palindromy
+
+def palindrome_alt(word, case_sensitive = False):
+    '''
+    This function checks wether given word is a palindrome, 
+    i.e. if there's no matter if it's read forwards or backwards, 
+    e.g. "kajak", using whole lists comparison.
+    The case_sensitive parameter tells if we care about upper- or lowercase.
+    '''
+    if case_sensitive == False:
+        word = word.casefold()
+    letters = [letter for letter in word]
+    letters_reversed = letters.copy()
+    letters_reversed.reverse()
+
+    return letters_reversed == letters
+
+print(palindrome_alt('Maoam', case_sensitive=False))
